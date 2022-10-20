8146b934d3bd49e74832e4cff107a79e81944987
황기연 <ghkdzm@gmail.com>
2022-10-20 17:09:56 +0900

    리액트 프록시 서버 추가, JPA--MySQL 연동, User데이터 추가

---
+++ src/main/java/com/example/fow/entity/User.java
@@ -0,0 +1,24 @@
+package com.example.fow.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    private String id; // 아이디
+
+    private String pw; // 비밀번호
+
+    private String nickname; // 닉네임
+
+    private String email; // 이메일
+
+    private String phnum; // 전화 번호
+
+    private String birth; // 생년월일
+}
---
+++ src/main/java/com/example/fow/service/UserService.java
@@ -0,0 +1,12 @@
+package com.example.fow.service;
+
+import com.example.fow.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+}
